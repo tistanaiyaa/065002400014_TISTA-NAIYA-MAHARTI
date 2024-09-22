fa40fcd9c4244945d60528d5ab1164e818770c45
tistanaiyaa <tistamaharti@gmail.com>
2024-09-22 21:43:46 +0700

    Add files via upload

---
+++ PRAK 1 A.py
@@ -0,0 +1,8 @@
+nama1 = input("nama kamu siapa?")
+asal1 = input("dari mana asal kamu?")
+sekolah1 = input("sebelumnya kamu bersekolah dimana?")
+
+
+print("halo selamat siang", nama1)
+print("wah dari", asal1)
+print("pasti menyenangkan di", sekolah1)
