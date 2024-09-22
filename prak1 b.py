6d9de9e99c20dba45dfca2522a152a65d173e1e8
tistanaiyaa <tistamaharti@gmail.com>
2024-09-22 21:34:30 +0700

    Add files via upload

---
+++ prak1 b.py
@@ -0,0 +1,10 @@
+print("hitung luas ruangan")
+panjang = float(input("masukkan panjang ruangan: "))
+lebar = float(input("masukkan lebar ruangan: "))
+satuan = input("masukkan satuan(meter/inci): ")
+luas = panjang*lebar
+print(luas)
+print(f"luas ruangan dengan panjang {panjang} dan lebar {lebar} adalah {luas} {satuan}.")
+
+
+
